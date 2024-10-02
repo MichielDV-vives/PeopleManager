8759d6b970048334f850babde016c24a28ea3253
Michiel De Vogelaere <michiel.devogelaere@gmail.com>
2024-10-02 10:34:23 +0200

    test_commit

---
+++ PeopleManager.Ui.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PeopleManager.Ui.Mvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
